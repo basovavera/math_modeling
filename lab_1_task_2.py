07473e2be57a82873592d0001d4df21c4492a62a
basovavera <98036369+basovavera@users.noreply.github.com>
2022-02-01 20:28:02 +0000

    дз

---
+++ lab_1_task_2.py
@@ -0,0 +1,8 @@
+x="Hello, World!"
+print('тип объекта 1:', type(x))
+x=3+4
+print('тип объекта 2:', type(x))
+x=3/4
+print('тип объекта 3:', type(x))
+x=[1, 2, 5, 10, 100]
+print('тип объекта 4:', type(x))
