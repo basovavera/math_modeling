8d0c48c28454909e00b0d9671bff4de20b6eaba6
basovavera <98036369+basovavera@users.noreply.github.com>
2022-02-08 18:50:23 +0000

    lec2

---
+++ lec_2_train.py
@@ -0,0 +1,21 @@
+a=3
+b=5
+c=-1
+if a>3 or c==0:
+  print('good')
+elif a<=3 and b!=5:
+  print('best')
+else:
+  print('bad')
+
+if a>=3 or b==0:
+  print('good')
+elif c==-1 or b>4:
+  print('best')
+else:
+  print('bad')
+
+if ((a==3 and c>0) or (b==5 and c<0)) and c == -1:
+    print('Good')
+else:
+    print('Bed')
